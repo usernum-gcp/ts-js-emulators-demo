e853a69ffec052307b4b469d4bc7cae709258e80
xi <ming.xi@logical-fabrics.com>
2020-04-04 19:00:24 +0800

    Update emulators.sh

--- emulators.sh
+++ emulators.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/ximing/Documents/test-functions-emulators-firebase-adminsdk-v0gvm-cb389aa0ae.json"
-firebase emulators:start
+# export GOOGLE_APPLICATION_CREDENTIALS="/Users/ximing/Documents/test-functions-emulators-firebase-adminsdk-v0gvm-cb389aa0ae.json"
+# firebase emulators:start
+
+
+# for durum seki
+# export GOOGLE_APPLICATION_CREDENTIALS="/Users/ximing/Documents/durum-develop-seki-firebase-adminsdk-fprrb-b0edf17152.json"
